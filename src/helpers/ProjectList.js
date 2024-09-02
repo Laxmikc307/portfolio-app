3b4a753f99fcf9f6f86c5f2c0655aa23bc04c712
Laxmikc307 <your_email@example.com>
2024-09-02 01:48:42 +0100

    projectlist done

---
+++ src/helpers/ProjectList.js
@@ -0,0 +1,36 @@
+import Image1  from "../assets/image1.png";
+import Image2  from "../assets/image2.png";
+import Image3  from "../assets/image3.png";
+
+export const ProjectList =[
+        {
+          id: 1,
+          name: "expense-tracker-app",
+          description:
+            "expense tracker app is a useful for track your income and expenses ",
+          image: Image1,
+          demo: "https://idyllic-duckanoo-11d320.netlify.app",
+          github: "https://github.com/laxmikckarki/expense-tracker-react",
+          type: "personal project",
+        },
+        {
+          id: 2,
+          name: "Usefulwebsites",
+          description:
+            "usefulwebsites is a web application that allows users to visit different websites from same page.",
+            image: Image2,
+            demo: "https://tubular-frangollo-33f523.netlify.app",
+          github: "https://github.com/laxmikckarki/usefulwebsites",
+          type: "personal project",
+        },
+        {
+          id: 3,
+          name: "cyf-hotel-react",
+          description:"The aim of the app is used for hotel booking as per clients need.",
+            image: Image3,
+            demo: "https://cyf-laxmikckarki-hotel-react.netlify.app/",
+          github: "https://github.com/laxmikckarki/cyf-hotel-react",
+          type: "personal project",
+        },];
+      
+      export default ProjectList;
